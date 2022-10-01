ded9f9dcf680ca1629d220692e9f5c52712d94ac
Tarek Mokadem <mokadem59200@live.fr>
2022-10-01 14:31:11 +0200

    Projet test

---
+++ eclipse-workspace/Projet/src/carre.java
@@ -0,0 +1,17 @@
+
+public class carre {
+
+	private int longueur;
+
+	public carre(int longueur) {
+		this.longueur = longueur;
+	}
+
+	public int getLongueur() {
+		return longueur;
+	}
+	
+	
+	
+	
+}
---
+++ eclipse-workspace/Projet/src/projet_momotata.java
@@ -0,0 +1,13 @@
+
+public class projet_momotata {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		carre c= new carre(5);
+		
+		System.out.println(c.getLongueur());
+		
+	}
+
+}
